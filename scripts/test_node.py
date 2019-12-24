13a2168e5cac5e89029eac6a23d1f9c98c946b89
Aaron <aaronh65@gmail.com>
2019-12-23 17:47:58 -0800

    beginning point goal navigation

--- scripts/test_node.py
+++ scripts/test_node.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import rospy
+from geometry_msgs.msg import Pose, PoseStamped
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
 
 def scanCallback(msg):
+
     if min(msg.ranges) > 1.0:
         ack_msg = AckermannDriveStamped()
         ack_msg.header.stamp = rospy.Time.now()
@@ -19,10 +21,21 @@ def scanCallback(msg):
         ack_msg.drive.speed = 0.0
         ack_publisher.publish(ack_msg)
 
+def goalCallback(msg):
+    goal = msg
+    origin = Pose()
+    origin.position.x = 0.0
+    origin.position.y = 0.0
+    origin.position.z = 0.0
+    pose_publisher.publish(origin)
+    print('published to origin')
+
 
 if __name__ == '__main__':
     rospy.init_node('test_node')
     rospy.Subscriber('scan', LaserScan, scanCallback)
+    rospy.Subscriber('move_base_simple/goal', PoseStamped, goalCallback)
     ack_publisher = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=1)
+    pose_publisher = rospy.Publisher('/pose', Pose, queue_size=1)
     while not rospy.is_shutdown():
         rospy.spin()
