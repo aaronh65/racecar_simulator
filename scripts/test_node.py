74b7ea1ce65489cd578c25eb86a1e1fbcdb944fc
Aaron <aaronh65@gmail.com>
2019-12-24 11:11:19 -0800

    initial test node

--- scripts/test_node.py
+++ scripts/test_node.py
@@ -3,6 +3,7 @@ import rospy
 from geometry_msgs.msg import Pose, PoseStamped
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 
 def scanCallback(msg):
 
@@ -21,6 +22,8 @@ def scanCallback(msg):
         ack_msg.drive.speed = 0.0
         ack_publisher.publish(ack_msg)
 
+def odomCallback(msg):
+    pass
 def goalCallback(msg):
     goal = msg
     origin = Pose()
@@ -35,6 +38,7 @@ if __name__ == '__main__':
     rospy.init_node('test_node')
     rospy.Subscriber('scan', LaserScan, scanCallback)
     rospy.Subscriber('move_base_simple/goal', PoseStamped, goalCallback)
+    rospy.Subscriber('odom', Odometry, odomCallback)
     ack_publisher = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=1)
     pose_publisher = rospy.Publisher('/pose', Pose, queue_size=1)
     while not rospy.is_shutdown():
